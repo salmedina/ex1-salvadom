c59241c17179beef28b1b4632953479e7d154acb
Salvador Medina <salvadom@andrew.cmu.edu>
2015-08-27 05:43:45 -0400

    Initial Commit: - Created empty Maven Project

---
+++ ex1-salvadom/src/main/java/edu/cmu/lti/f15ex1/ex1_salvadom/App.java
@@ -0,0 +1,13 @@
+package edu.cmu.lti.f15ex1.ex1_salvadom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
